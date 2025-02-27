5d7888e9d11d9711cbbfde60d74b35a3b024250e
supreeth1311 <supreeth1311@gmail.com>
2025-02-27 21:15:25 +0530

    config.py

--- config.py
+++ config.py
@@ -1,11 +1,4 @@
-# #import os
-# from dotenv import load_dotenv
-
-# load_dotenv()  # Load environment variables
-
-# GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")  # API Key from .env
 import os
-
 # Get API key from GitHub Secret (set during deployment)
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
